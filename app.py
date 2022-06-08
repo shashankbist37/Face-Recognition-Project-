02e4c16a4c509b72ed10bdb7ea74acad542a4adc
Shashank  Bisht <shashankbisht@Shashanks-MacBook-Air.local>
2022-06-08 11:09:06 +0530

    final

--- app.py
+++ app.py
@@ -17,22 +17,22 @@ def generatefunc():
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             for(x,y,w,h) in faces:
-                #                       
+                                    
                 cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),9)
                 roi_gray = gray[y:y+h, x:x+w]
                 roi_color = frame[y:y+h, x:x+w]
 
                 eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
-                smile = smile_cascade.detectMultiScale(roi_gray, 1.1, 1)
                 for (ex, ey, ew, eh) in eyes:
 
                         cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
+                smile = smile_cascade.detectMultiScale(roi_gray, 1.1,30 )
                 for (sx, sy, sw, sh) in smile:
 
                         cv2.rectangle(roi_color, (sx, sy), (sx+sw, sy+sh), (0, 0, 255), 2)        
 
-            ret,buffer=cv2.imencode('.jpg',frame)
+            res,buffer=cv2.imencode('.jpg',frame)
             frame=buffer.tobytes()
             
         yield(b'--frame\r\n'
